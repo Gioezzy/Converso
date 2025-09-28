fed819d75c248568da6cb8bb4254501b732c2f81
gioezzy <giovanniyuda29@gmail.com>
2025-09-28 23:29:34 +0700

    refactor: update Navbar with logo and navigation improvements

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,11 +4,11 @@ import NavItems from './NavItems';
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between mx-auto w-full px-14 py-4 bg-white max-sm:px-4">
+    <nav className="navbar">
       <Link href="/">
         <div className="flex items-center gap-2.5 cursor-pointer">
           <Image
-            src="images/logo.svg"
+            src="/images/logo.svg"
             alt="logo"
             width={46}
             height={44}
